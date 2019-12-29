5bf488483bce819f54609e901b859ca4a909f13e
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2019-12-29 00:01:28 -0300

    3.5 - Colecao vazia, o que retornar?

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,12 @@ public class CategoriaResource {
     private CategoriaRepository categoriaRepository;
 
    @GetMapping
-   public List<Categoria> listar() {
-        return categoriaRepository.findAll();
-
+    public List<Categoria> listar() {
+       return categoriaRepository.findAll();
+   /*
+   public ResponseEntity<?> listar() {
+          List<Categoria> categorias = categoriaRepository.findAll();
+        return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.notFound().build();
+    */
     }
 }
