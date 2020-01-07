66a75077d340bed5ed80499e9e24e20ff6b15366
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-01-07 18:21:16 -0300

    4.3 - Atualizando pessoa com PUT

---
+++ src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -0,0 +1,28 @@
+package com.example.algamoney.api.service;
+
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+        Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElse(null);
+        if (pessoaSalva == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+        pessoaRepository.save(pessoaSalva);
+        return pessoaRepository.save(pessoaSalva);
+
+
+    }
+}
