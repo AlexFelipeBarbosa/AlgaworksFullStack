38eefdba0fac9a502abea8ce0a83ce10acd8dc20
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-01-08 21:58:05 -0300

    5.3 - Desafio Cadastrando o primeiro lançamento

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -1,14 +1,16 @@
 package com.example.algamoney.api.resource;
 
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,9 +25,19 @@ public class LancamentoResource {
         return lancamentoRepository.findAll();
     }
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping("/{codigo}")
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
         Lancamento lancamento = lancamentoRepository.findById(codigo).orElse(null);
         return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
     }
 }
