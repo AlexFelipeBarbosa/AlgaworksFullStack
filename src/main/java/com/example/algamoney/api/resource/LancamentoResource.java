5190bf7eefeae88afdbdf95bbb8746b08e88b7ae
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-01-13 22:31:30 -0300

    5.7 - Implementando pesquisa de lançamento com Metamodel

--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -4,6 +4,7 @@ import com.example.algamoney.api.ExceptionHandler.AlgamoneyExceptionHandler;
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import com.example.algamoney.api.service.LancamentoService;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,9 @@ public class LancamentoResource {
     private MessageSource messageSource;
 
     @GetMapping
-    public List<Lancamento> listar() {
-        return lancamentoRepository.findAll();
+    public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+
+        return lancamentoRepository.filtrar(lancamentoFilter);
     }
 
     @Autowired
@@ -58,7 +60,7 @@ public class LancamentoResource {
         String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
         String mensagemDesenvolvedor = ex.toString();
         List<AlgamoneyExceptionHandler.Erro> erros = Arrays.asList(new AlgamoneyExceptionHandler.Erro(mensagemUsuario, mensagemDesenvolvedor));
-        return  ResponseEntity.badRequest().body(erros);
+        return ResponseEntity.badRequest().body(erros);
     }
 
 }
