79e182076e6cfcdf4f7f63f5f474b80a627dbb30
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-01-25 19:09:39 -0300

    7.2 - Profiles do Spring

--- src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.example.algamoney.api.cors;
 
+import com.example.algamoney.api.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,8 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-    private String originPermitida = "http://localhost:8000"; //TODO: Configurar para diferentes ambientes
+    @Autowired
+    private AlgamoneyApiProperty algamoneyApiProperty;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
@@ -22,10 +25,10 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setHeader("Acess-Control-Allow-Origin", originPermitida);
+        response.setHeader("Acess-Control-Allow-Origin", algamoneyApiProperty.getOriginPermitida());
         response.setHeader("Acess Control-Allow-Credentials", "true");
 
-        if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin")) ) {
+        if ("OPTIONS".equals(request.getMethod()) && algamoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             response.setHeader("Access-Control-Max-Age", "3600"); // 1 hora
@@ -34,7 +37,6 @@ public class CorsFilter implements Filter {
         } else {
             chain.doFilter(req, resp);
         }
-
 
 
     }
