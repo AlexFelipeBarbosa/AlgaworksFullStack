bfcd3cdeaee57628d341a20bf214c0a65820e517
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-01-25 12:16:45 -0300

    6.11 - Movendo o usuario para o banco de dados

--- src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -21,10 +24,17 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /* --> Alex Barbosa -- foi retirado a autenticação da memoria, a partir de agora a autenticação é no BD
         auth.inMemoryAuthentication()
                 // A partir da versão 2.1 do Spring é necessario colocar o {noop} no password
                 .withUser("admin").password("{noop}admin").roles("ROLE");
+                */
+
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -44,10 +54,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     /*
     @Override
     public void configure (ResourceServerSecurityConfigurer resources) throws Exception {
         resources.stateless(true);
     }
 */
+    //@Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
